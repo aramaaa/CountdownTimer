47fb152c619cc7a0a075405b795b36bd2d14a03c
araragu <aratsugugu@gmail.com>
2019-02-28 19:22:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/countdowntimer/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.countdowntimer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.CompletableFuture
+
+class MainActivity : AppCompatActivity() {
+
+    inner class MyCountDownTimer(millisInFuture: Long, countDownInterval: Long):
+        CountDownTimer(millisInFuture, countDownInterval){
+
+        var isRunning = false
+
+        override fun onTick(millisUntilFinished: Long) {
+            val minute = millisUntilFinished/1000L/60L
+            val second = millisUntilFinished/1000L%60L
+            timerText.text ="%1d:%2$02d".format(minute,second)
+        }
+        override fun onFinish(){
+            timerText.text = "0:00"
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        timerText.text = "3:00"
+        val timer = MyCountDownTimer(3*60*1000, 100)
+        playStop.setOnClickListener {
+            when(timer.isRunning){
+                true -> timer.apply{
+                    isRunning=false
+                    cancel()
+                    playStop.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+                }
+                false -> timer.apply {
+                    isRunning = true
+                    start()
+                    playStop.setImageResource(R.drawable.ic_stop_black_24dp)
+                }
+            }
+        }
+    }
+}
